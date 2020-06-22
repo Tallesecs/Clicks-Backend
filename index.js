9fa02f6fcf0cdd6046c6544fae78466f30f34234
tallesvsdi <talles@vsdi.com.br>
2020-06-21 21:14:07 -0300

    deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const clicks = require('./routes/clicks.routes'); // Importa rota
 const app = express();
 app.use('/clicks', clicks);
-let porto = 8000;
+let porto = process.env.PORT || 8000;
 app.listen(porto, () => {
     console.log('Servidor em execução no porto' + porto);
 });
